8807725dd24dd66360e3466703d8578a652687af
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-09 12:01:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h2>Bienvenidos al curso de React en CodingTube</h2>
+        </>
+    );
+}
+
+export default App;
