c5013e684bb14fcc8ecee1faa2fd48c3d737db03
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-09 23:12:12 -0300

    Props - children - ternarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
-import Header from "./Components/Header"
-import Section from "./Components/Section"
-import Footer from "./Components/Footer"
+import Header from "./Components/Header";
+import Section from "./Components/Section";
+import Footer from "./Components/Footer";
 
 function App() {
-
     return (
         <>
-            <Header/>
-            <Section/>
-            <Footer/>
+            <Header title1="Mi pagina web" show = {true}>
+                <h3>Este es el header (soy un children h3)</h3>
+            </Header>
+            <Section />
+            <Footer />
         </>
     );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,34 @@
 import './Header.css';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ children, title1, title2, show, show2}) => {
     return (
         <header className="header">
-            <h1>Header</h1>
+            <h1 className="title">{title1 || "No se recibio titúlo"}</h1>
+
+            <h2 className="title">
+                {title2 ||
+                    "No se recibio titúlo h2 (puedo ser un titulo por defecto)"}
+            </h2>
+
+            {children}
+
+            {show ? <p>Me muestro si show es True</p>: 
+                <p>Me muestro si SHOW is FALSE</p>
+            }
+
+            {show2 && <p>Me muestro si show2 es True</p>
+            }
+            {/* esto es igual a que el ternario devolviera null sino se cumple la primera condición */}
         </header>
     );
 }
+
+
+Header.prototype = {
+    children: PropTypes.node,
+    title: PropTypes.string,
+}
+
 
 export default Header
