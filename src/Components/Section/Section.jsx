34c9b2ce0563457a2d568fce51140e0787a1b1f2
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-19 23:08:57 -0300

    Estoy configurando la inserción de la imagenes de los usuarios

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,24 +1,48 @@
+import "../../assets"
+import "./Section.css";
 
-import "./section.css"
 
-let frutas = [ 'Mnazana', 'Banana', 'Melon' ]
+const users = [
+    {
+        id: 1,
+        nombre: "Carlos Morales",
+        description: "Web Fronted Developer Sr",
+    },
+    {
+        id: 2,
+        nombre: "Carlitos Morales",
+        description: "Web Fronted Developer Jr",
+    },
+    {
+        id: 3,
+        nombre: "Sofia Morales",
+        description: "Web Fronted Developer Training",
+    },
+    {
+        id: 4,
+        nombre: "Alberto Morales",
+        description: "Web Fronted Developer Ssr",
+    },
+];
 
 const Section = ({ title }) => {
-    return(
+    return (
         <section className="section">
-
             <h3>{title}</h3>
 
-            <ul>
-                {
-                    frutas.map( (fruta, index) => {
-                        return <li key={index}>{ fruta }</li>
-                    })
-                }
-            </ul>
+            {users.map((user) => {
 
+                const imagePath = require(`../../assets/${user.nombre}.png`).default
+                return (
+                    <div key={user.id}>
+                        <img src= {imagePath} alt= {user.nombre} />
+                        <h2>{user.nombre}</h2>
+                        <p>{user.description}</p>
+                    </div>
+                );
+            })}
         </section>
-    )
-}
+    );
+};
 
-export default Section
+export default Section;
