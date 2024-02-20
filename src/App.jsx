ca700b9f664387b955c59456a7f19961e4c32e4d
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-19 21:57:53 -0300

    Se separo en carpetas los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Header from "./Components/Header";
-import Section from "./Components/Section";
-import Footer from "./Components/Footer";
+import Header from "./Components/Header/Header";
+import Section from "./Components/Section/Section";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
     return (
