1289dfa0d9099e1498c82b95d4873a234d4d1fe0
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-03 23:46:47 -0300

    se implemento useState para arreglos y objetos

--- src/Components/Section/UserCard/UserCard.jsx
+++ src/Components/Section/UserCard/UserCard.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export const UserCard = ({ user }) => {
     const [isContacted, setIsContacted] = useState(false);
+    const [tecnologies, setTecnologies] = useState(['HTML', 'CSS', 'JS'])
+    const [addres, setAddres] = useState({street: 'Calle Falsa', number: 123})
 
     const { id, name, description, img } = user;
 
@@ -9,20 +11,32 @@ export const UserCard = ({ user }) => {
         setIsContacted(!isContacted);
     };
 
+    const handleClick2 = () => {
+        setTecnologies([...tecnologies, "React"]);
+        console.log(tecnologies);
+    };
+
+    const handleClick3 = () => {
+        setAddres({...addres, street: 'Nueva Calle', number: 456})
+        console.log(addres);
+    };
+
+    
+    // console.log(`Tarjeta ${name} renderizada`)
+
     return (
         <div key={id} className="card">
             <img src={img} alt={name} className="image" />
             <h2 className="name"> {name} </h2>
             <p className="description"> {description} </p>
-            <button
-                id={id}
-                onClick={() => {
-                    handleClick();
-                }}
-            >
-                {
-                    isContacted ? 'Contactado' : 'Contactar'
-                }
+            <button id={id} onClick={() => handleClick()}>
+                {isContacted ? "Contactado" : "Contactar"}
+            </button>
+            <button id={id} onClick={() => handleClick2()}>
+                Tecnologias
+            </button>
+            <button id={id} onClick={() => handleClick3()}>
+                Dirección
             </button>
         </div>
     );
